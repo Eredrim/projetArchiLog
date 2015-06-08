c0245973c9e74bd33689f2b326e17b6afafacce2
clem-62 <clem-62@tabclem>
2015-06-08 15:23:44 +0200

    Ajout de la classe Horloge.php

---
+++ Timezone/Horloge.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Horloge
+ *
+ * @author clem-62
+ */
+class Horloge {
+    private $fuseau;
+    private $ville;
+    private $format;
+    
+    function __construct($ville, $format = "H:i")
+    {
+        $fuseau = DateTimeZone($ville);
+        $this->ville = $ville;
+    }
+    
+    public function getHeure()
+    {
+        $date = new DateTime(null, $fuseau);
+        return $date->format($format);
+    }
+}
