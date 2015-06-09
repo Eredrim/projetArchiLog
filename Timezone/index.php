372de4460b706f3f4bb7d6ce98841567344b9382
Gilles <Gilles@Gilles-PC>
2015-06-09 14:43:11 +0200

    Ajout des horloges en grille

--- Timezone/index.php
+++ Timezone/index.php
@@ -8,13 +8,26 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/grid.css">
-        <link rel="stylesheet" type="text/css" href="css/global.css">
+        <script src="js/script.js"></script>
+        <script src="js/jquery-2.1.3.min.js"></script>
+        <!--<link rel="stylesheet" type="text/css" href="css/global.css">-->
         <title></title>
     </head>
     <body>
-        <div class="gridPiece" id="gp1">test</div>
+        <div class="gridPiece" id="gp1">
+            <div class="clock">
+                <div class="hour"></div>
+                <div class="minute"></div>
+            </div>
+        </div>
         <?php
-        // put your code here
+            include "Horloge.php";
+            $horloge1 = new Horloge("Europe/Paris");
+            $angleH = intval(explode(":",$horloge1->getHeure())[0])/12*360%360;
+            $angleM = intval(explode(":",$horloge1->getHeure())[1])/60*360;
+            echo '<script>$(".hour").css("transform", "rotate('.$angleH.'deg)");';
+            echo '$(".minute").css("transform", "rotate('.$angleM.'deg)");</script>';
         ?>
+        
     </body>
 </html>
